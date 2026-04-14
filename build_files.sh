05d4065200838af3c4333bf42c961f1fd0ccb43f
Elizaveta Gornovova <l.gornovova@yandex.ru>
2026-04-14 23:11:37 +0300

    psycopg2 doesn't install

--- build_files.sh
+++ build_files.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 echo "Build packages"
-python3.14 -m pip install --break-system-packages -r requirements.txt
+python3 -m pip install --break-system-packages -r requirements.txt
 
 cd knockdown/
 
 echo "Making migrations"
-python3.14 manage.py makemigrations users
-python3.14 manage.py makemigrations lessons
-python3.14 manage.py makemigrations stats
-python3.14 manage.py makemigrations
+python3 manage.py makemigrations users
+python3 manage.py makemigrations lessons
+python3 manage.py makemigrations stats
+python3 manage.py makemigrations
 
 echo "Migrating database"
-python3.14 manage.py migrate users
-python3.14 manage.py migrate lessons
-python3.14 manage.py migrate stats
-python3.14 manage.py migrate
+python3 manage.py migrate users
+python3 manage.py migrate lessons
+python3 manage.py migrate stats
+python3 manage.py migrate
 
 echo "Collecting static files"
-python3.14 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 echo "Populating dictionary"
-python3.14 scripts/populate_dictionary.py
+python3 scripts/populate_dictionary.py
