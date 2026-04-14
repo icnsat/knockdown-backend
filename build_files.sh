249cc5182f8b7035161a4fbcc4f859a15841294b
Elizaveta Gornovova <l.gornovova@yandex.ru>
2026-04-14 22:56:13 +0300

    deleted static route

--- build_files.sh
+++ build_files.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 echo "Build packages"
-python3 -m pip install --break-system-packages -r requirements.txt
+python3.14 -m pip install --break-system-packages -r requirements.txt
 
 cd knockdown/
 
 echo "Making migrations"
-python3 manage.py makemigrations users
-python3 manage.py makemigrations lessons
-python3 manage.py makemigrations stats
-python3 manage.py makemigrations
+python3.14 manage.py makemigrations users
+python3.14 manage.py makemigrations lessons
+python3.14 manage.py makemigrations stats
+python3.14 manage.py makemigrations
 
 echo "Migrating database"
-python3 manage.py migrate users
-python3 manage.py migrate lessons
-python3 manage.py migrate stats
-python3 manage.py migrate
+python3.14 manage.py migrate users
+python3.14 manage.py migrate lessons
+python3.14 manage.py migrate stats
+python3.14 manage.py migrate
 
 echo "Collecting static files"
-python3 manage.py collectstatic --noinput
+python3.14 manage.py collectstatic --noinput --clear
 
 echo "Populating dictionary"
-python3 scripts/populate_dictionary.py
+python3.14 scripts/populate_dictionary.py
--- knockdown/knockdown/settings.py
+++ knockdown/knockdown/settings.py
@@ -178,7 +178,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Папка, куда collectstatic собирает файлы
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
