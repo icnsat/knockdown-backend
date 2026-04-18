d1fe127f999d73ace6a4f6082bc247022790c37f
Elizaveta Gornovova <l.gornovova@yandex.ru>
2026-04-18 15:22:27 +0300

    added new build file

---
+++ knockdown/vercel-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "Installing dependencies"
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+echo "Making migrations"
+python3 manage.py makemigrations users
+python3 manage.py makemigrations lessons
+python3 manage.py makemigrations stats
+python3 manage.py makemigrations
+
+echo "Migrating database"
+python3 manage.py migrate users
+python3 manage.py migrate lessons
+python3 manage.py migrate stats
+python3 manage.py migrate
+
+echo "Collecting static files"
+python3 manage.py collectstatic --noinput --clear
+
+echo "Populating dictionary"
+python3 scripts/populate_dictionary.py
